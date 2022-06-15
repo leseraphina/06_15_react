deeae72932fc0b15dbd236387c94c902f35026a9
leseraphina <leseraphina@gamil.com>
2022-06-15 21:40:34 +0900

    06_15

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React,{useState,useEffect,useCallback} from 'react'
+import ReactDOM from 'react-dom/client'
+// component
+import AddApointment from './Component/AddApointment'
+import Seach from './Component/Search'
+import AddInfo from './Component/AddInfo'
+// source
+import './index.css'
+import {BiArchive} from 'react-icons/bi'
+// import appointData from './data.json'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// App
+function App(){
+  //  state 설정하기
+  let [appointList,setAppoinList] = useState([])
+  //  callback 설정하기
+  const fetData = useCallback(
+    ()=>{
+      fetch('./data.json')
+      .then(response => response.json())
+      .then(data => setAppoinList(data))
+    }
+  )
+  // userEffect설정하기
+  useEffect(() => {fetData()},[fetData])
+  return (
+    <article>
+      <h3><BiArchive style={{color:'#d47776'}} />welcome</h3>
+      <AddApointment />
+      <Seach />
+      <div id="list">
+        <ul>
+          {appointData.map( (item) => (
+            <AddInfo 
+            key= {item.id}
+            appointment ={item}/>
+          ))}
+          {/* <AddInfo /> */}
+        </ul>
+      </div>
+    </article>
+  )
+}
+
+// 출력
+const root = ReactDOM.createRoot(document.querySelector('#root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
